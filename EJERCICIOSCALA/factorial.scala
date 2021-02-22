9c4635217b0878539f0d8063de3d9a03299fda04
Alberto Montes Ramos <68075328+MOHATAN@users.noreply.github.com>
2021-02-22 15:08:46 +0100

    Update factorial.scala

--- EJERCICIOSCALA/factorial.scala
+++ EJERCICIOSCALA/factorial.scala
@@ -1,15 +1,15 @@
-def factorial(n: Int): Int = {  
+def factorial(n: BigInt): BigInt = {  
         var f = 1
         for(i <- 1 to n)
         {
             f = f * i;
         }
         return f
-    }                                             //> factorial: (n: Int)Int
+    }                                             //> factorial: (n: BigInt)BigInt
     factorial(5)                                 //> res4: Int = 120
 
 /***con recursividad******/
- def factorial(n: Int): Int =
+ def factorial(n: BigInt): BigInt =
     { 
         if (n == 0)  
             return 1
